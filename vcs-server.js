a16a2e59ca78ed3c1dc2d216ae44ff17eab07ef0
ArchKeyTech <50621192+ArchKeyTech@users.noreply.github.com>
2022-01-11 01:27:36 +0200

    Update vcs-server.js

--- vcs-server.js
+++ vcs-server.js
@@ -21,9 +21,9 @@ const gitlab_token = "glpat-xyRszvf5c3qGuiK4vJy4";
 
 if (process.env.NODE_ENV === 'production'){
   
-  app.use(express.static(path.join(__dirname, '/frontend/public')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/public", "index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
   });
 }
 
