9ecea39b8b1393a7d9cb53ca8eed3a5b0ad337ef
ArchKeyTech <50621192+ArchKeyTech@users.noreply.github.com>
2022-01-11 01:10:28 +0200

    Update vcs-server.js

--- vcs-server.js
+++ vcs-server.js
@@ -18,11 +18,14 @@ require("isomorphic-fetch");
 
 const gitlab_token = "glpat-xyRszvf5c3qGuiK4vJy4";
 
+
+app.use(express.static(publicPath));
+
 if (process.env.NODE_ENV === 'production'){
   
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, '/frontend/public')));
   app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/public", "index.html"));
   });
 }
 
